0e795c3841f486256cdc9ba44a66adfed01612a2
unknown <Administrador@7-3Sailin.mincin.gob.cu>
2022-07-29 11:13:41 -0400

    changed home page removed Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+  
+  cities= ['barcelona','madrid','lima'];
+  name!:string;
   title = 'reto05';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change->', changes);
+  }
+    ngOnInit(): void {
+      console.log('OnInit->');
+    }
+ 
+  ngOnDestroy(): void {
+    console.log('destroy');
+  }
+
+
+
 }
+
